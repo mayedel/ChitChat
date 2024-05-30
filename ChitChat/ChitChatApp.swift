1828b04a332b7803b266c8701eed1d06745b50a5
María Espejo <mayed92@hotmail.com>
2024-05-30 20:02:56 +0200

    Initial commit

---
+++ ChitChat/ChitChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChitChatApp.swift
+//  ChitChat
+//
+//  Created by María Espejo on 30/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ChitChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
